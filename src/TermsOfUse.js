84fe748c1058026f76c15bc83e766545d8c5cdc0
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-03-25 16:46:13 -0500

    File Created

---
+++ src/TermsOfUse.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TermsOfUse extends React.Component {
+    render() {
+        return (
+            <div>
+                Terms Of Use
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default TermsOfUse;
