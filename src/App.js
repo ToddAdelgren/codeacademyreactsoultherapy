389c47dfa211f0de0ac38e0af4432e132b8fa2d9
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-03-25 16:15:22 -0500

    Inital Programming Related To Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       React Soul Therapy
+      <Footer />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
 class Footer extends React.Component {
     render() {
         return (
-            <div>
-                Page Footer
-            </div>
+            <Router>
+                <div className="container-fluid bt ht4">
+                    <div className="row text-center mt-4">
+                        <div className="col-4">
+                            Copy Right 2020
+                        </div>
+                        <div className="col-4">
+                            <Link to="">Terms Of Use</Link>
+                        </div>
+                        <div className="col-4">
+                            <Link to="">Privacy Policy</Link>
+                        </div>
+                    </div>
+                </div>
+            </Router>
         );
     }
 }
+
+export default Footer;
