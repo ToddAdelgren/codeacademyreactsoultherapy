9ab5d2b0dd85416fc286430a8ea60f5324d8cf55
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-03-25 17:06:28 -0500

    Added Initial Content

---
+++ src/PrivacyPolicy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PrivacyPolicy extends React.Component {
+    render() {
+        return (
+            <div>
+                Privacy Policy
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default PrivacyPolicy;
